4c88179a16508519eedf94aeb206b696cf8fa5bc
Kat <katlin7@gmail.com>
2023-10-28 15:53:22 +0300

    added displaying one product,changed product. services.ts, db.json & models/IProduct.ts interface

--- models/IProduct.ts
+++ models/IProduct.ts
@@ -1,4 +1,5 @@
 export interface IProduct{
+    id?:number;
     title:string;
     count:number;
     price?:number;
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -11,7 +11,12 @@ export class ProductService {
   constructor(private httpClient: HttpClient) { }
 
   getProducts(): Observable<Array<IProduct>>{
-    //
+    //This gets the JSON with products:  curl -X GET http://localhost:3000/products
     return this.httpClient.get<Array<IProduct>>(this.productsUrl);
+  }
+
+  getProductById(id: number): Observable<IProduct> {
+    //This gets the JSON with a product:  curl -X GET http://localhost:3000/products/3
+    return this.httpClient.get<IProduct> (`${this.productsUrl}/${id}`);
   }
 }
